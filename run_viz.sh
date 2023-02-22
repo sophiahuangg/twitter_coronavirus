81cb8ecdc6b0402cc210febbfb57904284976a7c
Sophia Huang <shuang24@students.claremontmckenna.edu>
2023-02-21 16:44:50 -0800

    Plots For All Hashtags

--- run_viz.sh
+++ run_viz.sh
@@ -2,9 +2,8 @@
 
 for file in reduced*
 do
-    for hashtag in \'#coronavirus\' \'#코로나바이러스\'
+    for hashtag in '#coronavirus' '#코로나바이러스' '#コロナウイルス' '#冠状病毒' '#covid2019' '#covid-2019' '#covid19' '#covid-19' '#corona' '#virus' '#flu' '#sick' '#cough' '#sneeze' '#hospital' '#nurse' '#doctor'
     do
-#    nohup ./src/visualize.py --input_path="$file" --key=$hashtag > visualizations/$hashtag &
-    echo Looping through $hashtag
+    nohup ./src/visualize.py --input_path="$file" --key=$hashtag > vizcheck/"$file"."$hashtag" &
     done
 done
--- src/visualize.py
+++ src/visualize.py
@@ -1,27 +1,6 @@
 #!/usr/bin/env python3
 
 import matplotlib
-matplotlib.use('Agg')
-
-hashtags = [
-    '#코로나바이러스',  # korean
-    '#コロナウイルス',  # japanese
-    '#冠状病毒',        # chinese
-    '#covid2019',
-    '#covid-2019',
-    '#covid19',
-    '#covid-19',
-    '#coronavirus',
-    '#corona',
-    '#virus',
-    '#flu',
-    '#sick',
-    '#cough',
-    '#sneeze',
-    '#hospital',
-    '#nurse',
-    '#doctor',
-    ]
 
 # command line args
 import argparse
@@ -29,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--input_path',required=True)
 parser.add_argument('--key',required=True)
 parser.add_argument('--percent',action='store_true')
+parser.add_argument('--output_folder',default='plots')
 args = parser.parse_args()
 
 # imports
@@ -52,8 +32,8 @@ for k,v in items:
     print(k,':',v)
 
 # plotting
-topten = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse = True)[:10]
-print("TOP 10=", topten)
+topten = sorted(sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)[:10], key=lambda x: x[1])
+print("Top 10=", topten)
 
 key = []
 value = []
@@ -62,13 +42,36 @@ for k, v in topten:
     key.append(k)
     value.append(v)
 
-print("KEY=", key)
-print("VALUE=", value)
+print("Key=", key)
+print("Value=", value)
 
-plt.bar(key, sorted(value), color = 'maroon', width = 0.5)
+plt.bar(key, value, color = 'maroon', width = 0.5)
 
-plt.xlabel("Language")
-plt.ylabel("Y Value")
+# Depending on whether the input path is for countries or languages, decide what to name x axis and title of plot
+if args.input_path == "reduced.country":
+    plt.xlabel("Country")
+    plt.ylabel("Num")
+    plt.title("Usage of " + args.key + " in Countries")
+else:
+    plt.xlabel("Language")
+    plt.ylabel("Num")
+    plt.title("Usage of " + args.key + " in Languages")
 
-plt.savefig('코로나바이러스_language.png')
+try:
+    os.makedirs(args.output_folder)
+except FileExistsError:
+    pass
 
+# Create files in plots folder for each hashtag
+output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path) + '.' + os.path.basename(args.key))
+
+print("os.path.basename(args.input_path=)", os.path.basename(args.input_path))
+print("os.path.basename(args.key)=", os.path.basename(args.key))
+print("output_path_base=", output_path_base)
+
+
+# Saving plot to plots folder
+try:
+    plt.savefig(output_path_base+'.png')
+except:
+    print("HAVING ERROR SAVING")
